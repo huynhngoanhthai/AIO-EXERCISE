e9195bcbc03ca3ddeb50e6700068fd407bb91386
Huỳnh Ngô Anh Thái <74443669+huynhngoanhthai@users.noreply.github.com>
2024-07-20 16:45:24 +0700

    Feat: Implement fundamental vector and matrix operations
    This commit introduces a new module providing essential functions for vector and matrix manipulations. These functions include calculating vector length, dot product, matrix multiplication, matrix inverse, eigenvalues and eigenvectors, and cosine similarity.
    
    The addition of these functions lays the groundwork for more complex linear algebra computations and applications within the project.

---
+++ module-2/week2/VectorOperations.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+# a
+
+
+def compute_vector_length(vector: np.array) -> float:
+
+    len_of_vector = np.linalg.norm(vector)
+
+    return len_of_vector
+
+
+# b
+
+def compute_dot_product(vector1: np.array, vector2: np.array) -> float:
+
+    result = np.dot(vector1, vector2)
+
+    return result
+
+
+# c
+
+def matrix_multi_vector(matrix: np.array, vector: np.array) -> np.array:
+
+    result = np.dot(matrix, vector)
+
+    return result
+
+
+# d
+
+def matrix_multi_matrix(matrix1: np.array, matrix2: np.array) -> np.array:
+
+    result = np.dot(matrix1, matrix2)
+
+    return result
+
+# e
+
+
+def inverse_matrix(matrix: np.array) -> np.array:
+    if matrix.shape != (2, 2):
+        raise ValueError("Ma trận phải là ma trận 2x2.")
+
+    a, b = matrix[0]
+    c, d = matrix[1]
+
+    det = a * d - b * c
+
+    if det == 0:
+        raise ValueError("Ma trận không khả nghịch (det(A) = 0).")
+
+    result = (1 / det) * np.array([[d, -b], [-c, a]])
+
+    return result
+
+
+# F
+def compute_eigenvalues_eigenvectors(matrix: np.array):
+    eigenvalues, eigenvectors = np.linalg.eig(matrix)
+
+    return eigenvalues, eigenvectors
+
+# G
+
+
+def compute_cosine(v1, v2):
+    cos_sim = (compute_dot_product(v1, v2)) / \
+        (compute_vector_length(v1)*compute_vector_length(v2))
+    return cos_sim
+
+
+x = np. array([1, 2, 3, 4])
+y = np. array([1, 0, 3, 0])
+result = compute_cosine(x, y)
+print(round(result, 3))
